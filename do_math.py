efc09db01351fde20f29ddbbe54a3d0bc4516635
Akshay Trivedi <aku24.7x3@gmail.com>
2024-10-27 12:45:23 -0400

    odd a

--- do_math.py
+++ do_math.py
@@ -1,64 +1,75 @@
 import argparse
 
-def ctz(n):
-    return (n & -n).bit_length() - 1
+def lsb(x):
+    return x & -x
 
-def order_of_a_mod_2n(a, n):
-    x = a
-    k = 1
-    while x != 1:
-        x = (x * x) % n
-        k <<= 1
-    return k
+def ctz(n):
+    return lsb(n).bit_length() - 1
 
 def fast_exp(a, b, n):
-    ret = 1
-    apow = a
+    ret, apow = 1, a
     for bit in f"{b:b}"[::-1]:
-        if int(bit) == 1:
-            ret = (ret * apow) % n
-        apow = (apow * apow) % n
+        if int(bit):
+            ret = (ret * apow) % (1 << n)
+        apow = (apow * apow) % (1 << n) 
     return ret
 
+def gen_seq(b):
+    a, n = (lsb(b), ctz(b) + 1)
+    while True:
+        yield a, n
+        n += (b - 1) * ctz(b)
+        a = b ^ fast_exp(a, b, n)
+
+# a xor b = a^b
+
 def solve_for_a(b, n):
-    if b == 0:
-        if n == 1:
-            return [0]
-        else:
-            return []
-    elif b % 1:
-        return []
-    else:
-        raise Exception("TODO")
+    if b % 2:
+        return
+    l = []
+    for a_0, n_0 in gen_seq(b):
+        if n_0 >= n:
+            l.append(a_0 % (1 << n))
+            break
+    a = 1
+    for k in range(1, n+1):
+        a = (b ^ fast_exp(a, b, k)) % (1 << k)
+    l.append(a)
+    yield from sorted(l)
 
 def solve_for_b(a, n):
-    if n == 1:
-        return [0]
+    if n == 0:
+        yield 0
     elif a % 2:
-        k = order_of_a_mod_2n(a, n)
-        b = solve_for_b(a % k, k)[0]
-        return [a ^ fast_exp(a, b, n)]
+        b = 0
+        for k in range(1, n+1):
+            b = (a ^ fast_exp(a, b, k)) % (1 << k)
+        yield b
     else:
         l = []
-        if fast_exp(a, a, n) == 0:
-            l.append(a)
         apow = 1
-        for b in range(0, ctz(n), 2):
+        for b in range(0, n, 2):
             if apow == a ^ b:
-                l.append(b)
-            apow = (a * a * apow) % n
-        return l
-                
+                yield b
+            apow = (a * a * apow) % (1 << n)
+        if a >= n and fast_exp(a, a, n) == 0:
+            yield a
 
 def solve_for_n(a, b):
-    if b % 1:
-        return []
+    if b % 2:
+        return
+    n = 0
+    for (a_0, n_0) in gen_seq(b):
+        while b < (1 << n_0) and n <= n_0:
+            if a == a_0 % (1 << n):
+                yield n
+            n += 1
     else:
         raise Exception("TODO: a and b")
 
 
 def solve_for_ab(n):
-    return [(a, b) for a in range(n) for b in solve_for_b(a, n)]
+    yield from ((a, b) for a in range(n) for b in solve_for_b(a, n))
 
 def solve_for_an(b):
     raise Exception("TODO: only b")
@@ -66,25 +77,24 @@ def solve_for_an(b):
 def solve_for_bn(a):
     raise Exception("TODO: only a")
 
-def run(a=None, b=None, n=None):
-    
+def iterate(a=None, b=None, n=None):
     if a != None and b != None and n != None:
         if fast_exp(a, b, n) != a ^ b:
-            return []
+            return
         else:
-            return [(a, b, n)]
+            yield (a, b, n)
     elif a != None and b != None:
-        return [(a, b, n) for n in solve_for_n(a, b)]
+        yield from ((a, b, n) for n in solve_for_n(a, b))
     elif a != None and n != None:
-        return [(a, b, n) for b in solve_for_b(a, n)]
+        yield from ((a, b, n) for b in solve_for_b(a, n))
     elif b != None and n != None:
-        return [(a, b, n) for a in solve_for_a(b, n)]
+        yield from ((a, b, n) for a in solve_for_a(b, n))
     elif a != None:
-        return [(a, b, n) for (b, n) in solve_for_bn(a)]
+        yield from ((a, b, n) for (b, n) in solve_for_bn(a))
     elif b != None:
-        return [(a, b, n) for (a, n) in solve_for_an(b)]
+        yield from ((a, b, n) for (a, n) in solve_for_an(b))
     elif n != None:
-        return [(a, b, n) for (a, b) in solve_for_ab(n)]
+        yield from ((a, b, n) for (a, b) in solve_for_ab(n))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find all solutions to `a XOR b = a POW b  (MOD n)`.")
@@ -97,19 +107,15 @@ if __name__ == "__main__":
     if a == None and b == None and n == None:
         parser.error("at least one of '-a', '-b', '-n', must be specified")
     if n != None:
-        if not (n >= 0 and n.bit_count() == 1):
-            parser.error(f"argument 'n': must be power of 2, got {n}")
-        if a != None and not (0 <= a < n):
+        if not (n >= 0):
+            parser.error(f"argument 'n': must be a nonnegative integer, got {n}")
+        if a != None and not (0 <= a < (1<<n)):
             parser.error(f"argument 'a': must be in [0, n), got a={a}, n={n}")
-        if b != None and not (0 <= b < n):
+        if b != None and not (0 <= b < (1<<n)):
             parser.error(f"argument 'b': must be in [0, n), got b={b}, n={n}")
 
-    l = run(a=a, b=b, n=n)
-    az, bz, nz = 0, 0, 0
+    l = iterate(a=a, b=b, n=n)
+    print("a,b,n")
     for a, b, n in l:
-        az, bz, nz = max(az, len(str(a))), max(bz, len(str(b))), max(nz, len(str(n)))
-    print(f"| {'a':<{az}} | {'b':<{bz}} | {'n':<{nz}} |")
-    print(f"|-{'-':<{az}}-|-{'-':<{bz}}-|-{'-':<{nz}}-|")
-    for a, b, n in l:
-        print(f"| {a:<{az}} | {b:<{bz}} | {n:<{nz}} |")
+        print(f"{a},{b},{n}")
 
